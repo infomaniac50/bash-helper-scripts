76bf69beda65fd4072f9524cb58378a945cdad95
Derek Chafin <infomaniac50@gmail.com>
2015-01-02 11:04:17 -0600

    I'm the only user on my machine so just notify the first user found for DBUS.

--- libnotify/notify-send-as-root.sh
+++ libnotify/notify-send-as-root.sh
@@ -8,18 +8,21 @@
 # process to determine DBUS_SESSION_BUS_ADDRESS
 USER_DBUS_PROCESS_NAME="gconfd-2"
 
-USER="makubi"
 NOTIFY_SEND_BIN="/usr/bin/notify-send"
 
 TITLE="title"
 MESSAGE="notify message"
 
-# get pid of user dbus process
-DBUS_PID=`ps ax | grep $USER_DBUS_PROCESS_NAME | grep -v grep | awk '{ print $1 }'`
+DBUS_PID_LINE=`ps aux | grep $USER_DBUS_PROCESS_NAME | grep -v grep`
+
+# get user of first dbus process
+USER=`echo $DBUS_PID_LINE | awk '{ print $1 }'`
+
+# get pid of first user dbus process
+DBUS_PID=`echo $DBUS_PID_LINE | awk '{ print $2 }'`
 
 # get DBUS_SESSION_BUS_ADDRESS variable
 DBUS_SESSION=`grep -z DBUS_SESSION_BUS_ADDRESS /proc/$DBUS_PID/environ | sed -e s/DBUS_SESSION_BUS_ADDRESS=//`
 
 # send notify
 DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION su -c "$NOTIFY_SEND_BIN \"$TITLE\" \"$MESSAGE\"" $USER
-
